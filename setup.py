53cf93b64f2d6066c0186fc555ec260c401a9b01
Richard Tjörnhammar <98884156+rictjo@users.noreply.github.com>
2022-12-14 13:44:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name         = "biocarta",
+    version      = "0.1.0",
+    author       = "Richard Tjörnhammar",
+    author_email = "richard.tjornhammar@gmail.com",
+    description  = "",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/rictjo/biocarta",
+    packages = setuptools.find_packages('src'),
+    package_dir = {'biocarta':'src/biocarta','quantification':'src/quantification','special':'src/special'},
+    classifiers = [
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
