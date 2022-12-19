1d13e641d7543522eed2c7aef9f3971967e51ff9
Richard Tjörnhammar <98884156+rictjo@users.noreply.github.com>
2022-12-19 10:43:30 +0100

    Add files via upload

--- src/biocarta/quantification.py
+++ src/biocarta/quantification.py
@@ -28,7 +28,7 @@ def create_mapping ( distm:np.array , cmd:str = 'max' ,
                      local_connectivity:float = 20 ,
                      transform_seed:int = 42 ) -> tuple[pd.DataFrame] :
 
-    clabels_n , clabels_o , hierarch_df, screening = generate_clustering_labels ( distm ,
+    clabels_n , clabels_o , hierarch_df, sol = generate_clustering_labels ( distm ,
             labels = index_labels , cmd = cmd , n_clusters = n_clusters ,
             bExtreme = bExtreme )
     if MF is None : # IF NOT PRECOMPUTED
@@ -41,7 +41,7 @@ def create_mapping ( distm:np.array , cmd:str = 'max' ,
                                       n_components            = umap_dimension     ,
                                       n_neighbors             = n_neighbors        ,
                                       transform_seed          = transform_seed     ,
-                                        ).fit_transform( Xf ) )
+                                        ).fit_transform( Xf ) ) #distm ) )
     resdf = pd.DataFrame( Xf ,
                   index      = index_labels ,
                   columns    = [ 'MFX.'+str(i) for i in range( len(Xf.T) ) ] )
@@ -55,22 +55,32 @@ def create_mapping ( distm:np.array , cmd:str = 'max' ,
     return ( resdf, hierarch_df , pd.DataFrame(sol) )
 
 def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
-        bVerbose = False , bExtreme = True , force_n_clusters = None ,
-        n_components = None , bDoUmap = True ,
+        bVerbose:bool = False , bExtreme:bool = True , force_n_clusters:int = None ,
+        n_components:int = None , bDoUmap:bool = True ,
         distance_type:str = 'correlation,spearman,absolute' ,
         umap_dimension:int = 2 , umap_n_neighbors:int = 20 , umap_local_connectivity:float = 20.,
-        umap_seed = 42, hierarchy_cmd = 'max' , add_labels=None ) -> tuple[pd.DataFrame]:
+        umap_seed:int = 42 , hierarchy_cmd:str = 'max' ,
+        add_labels:list[str] = None, directory:str = None ) -> tuple[pd.DataFrame] :
     #
+    if bVerbose :
+        import time
+        header_str = 'YMDHMS_'+'_'.join( list( str(t) for t in time.gmtime())[:-3] )
+        if not directory is None :
+            if not directory[-1] == '/' :
+                directory = directory + '/'
+            header_str = directory + header_str
+
     adf = adf.iloc[ np.inf != np.abs( 1.0/np.std(adf.values,1) ) ,
                     np.inf != np.abs( 1.0/np.std(adf.values,0) ) ].copy()
-    jdf = jdf.loc[:,adf.columns.values.tolist() ].copy()
+    jdf = jdf .loc[ :,adf.columns.values.tolist() ].copy()
     #
     n_neighbors = umap_n_neighbors
     local_connectivity = umap_local_connectivity
     transform_seed = umap_seed
     cmd = hierarchy_cmd
     #
-    u , s , vt = np.linalg.svd ( adf.values , False )
+    from impetuous.special import zvals
+    u , s , vt = np.linalg.svd ( zvals( adf.values )['z'] , False )
     #
     MF_f = u*s
     MF_s = vt.T*s
@@ -90,8 +100,8 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
     #
     if bVerbose :
         print ( 'STORING RESULTS 1 > ', 'resdf_f.tsv' )
-        resdf_f .to_csv( 'resdf_f.tsv',sep='\t' )
-        soldf_f .to_csv( 'soldf_f.tsv',sep='\t' )
+        resdf_f .to_csv( header_str + 'resdf_f.tsv',sep='\t' )
+        soldf_f .to_csv( header_str + 'soldf_f.tsv',sep='\t' )
     #
     distm_samples  = distance_calculation ( adf.T.values, distance_type , bRemoveCurse = True )
     resdf_s , hierarch_s_df , soldf_s = create_mapping ( distm = distm_samples ,
@@ -102,8 +112,8 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
                      transform_seed = transform_seed )
     if bVerbose :
         print ( 'STORING RESULTS 2 > ', 'resdf_s.tsv' )
-        resdf_s .to_csv( 'resdf_s.tsv',sep='\t' )
-        soldf_s .to_csv( 'soldf_s.tsv',sep='\t' )
+        resdf_s .to_csv( header_str + 'resdf_s.tsv',sep='\t' )
+        soldf_s .to_csv( header_str + 'soldf_s.tsv',sep='\t' )
     #
     pcas_df , pcaw_df = None , None
     if not jdf is None :
@@ -114,12 +124,13 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
                     n_components = n_components , add_labels = add_labels )
     if bVerbose :
         print ( 'STORING RESULTS 3,4 > ', 'pcas_df.tsv', 'pcaw_df.tsv' )
-        pcas_df .to_csv( 'pcas_df.tsv','\t' )
-        pcaw_df .to_csv( 'pcaw_df.tsv','\t' )
+        pcas_df .to_csv( header_str + 'pcas_df.tsv','\t' )
+        pcaw_df .to_csv( header_str + 'pcaw_df.tsv','\t' )
         print ( 'STORING RESULTS 5,5 > ', ' hierarch_f.tsv', ' hierarch_s.tsv' )
-        hierarch_s_df.to_csv('hierarch_s.tsv',sep='\t')
-        hierarch_f_df.to_csv('hierarch_f.tsv',sep='\t')
-    #
+        hierarch_s_df.to_csv( header_str + 'hierarch_s.tsv' , sep='\t' )
+        hierarch_f_df.to_csv( header_str + 'hierarch_f.tsv' , sep='\t' )
+    #map_df_f =
+    #map_df_s =
     return ( resdf_f , hierarch_f_df, pcas_df , resdf_s , hierarch_s_df , pcaw_df )
 
 
@@ -170,6 +181,7 @@ if __name__ == '__main__' :
     distance_type = 'correlation,spearman,squared'
     # distance_type = 'euclidean'
     #
+    print ( 'DOING THIS' , bVerbose )
     full_mapping ( adf , jdf ,
         bVerbose = bVerbose , bExtreme = bExtreme , force_n_clusters = n_clusters ,
         n_components = n_components , bDoUmap = bDoUmap ,
