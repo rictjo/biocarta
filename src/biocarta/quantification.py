d5775a26fc967e5a688517db7e51d528cca042e6
Richard Tjörnhammar <98884156+rictjo@users.noreply.github.com>
2022-12-20 21:31:56 +0100

    Add files via upload

--- src/biocarta/quantification.py
+++ src/biocarta/quantification.py
@@ -29,17 +29,19 @@ def create_mapping ( distm:np.array , cmd:str	= 'max'	,
                      n_proj:int			= 2	,
                      local_connectivity:float	= 20	,
                      transform_seed:int = 42 ) -> tuple[pd.DataFrame] :
-
+    #
     clabels_n , clabels_o , hierarch_df, sol = generate_clustering_labels ( distm ,
             labels = index_labels , cmd = cmd , n_clusters = n_clusters ,
             bExtreme = bExtreme )
-
+    #
+    fully_connected_at = float( hierarch_df.index.values[-1] )
     if n_proj is None :
         n_proj = len(distm)
-
+    #
     if MF is None : # IF NOT PRECOMPUTED
-        Xf = distance_matrix_to_absolute_coordinates ( distm ,
-			n_dimensions = -1 , bLegacy = False )
+        Xf = distm * ( distm <= fully_connected_at )
+        # distance_matrix_to_absolute_coordinates ( distm ,
+        #    n_dimensions = -1 , bLegacy = False ) # WASTE OF COMPUTATION
     else :
         Xf = MF
     #
@@ -50,13 +52,18 @@ def create_mapping ( distm:np.array , cmd:str	= 'max'	,
                                       transform_seed		= transform_seed	,
                                         )\
 					.fit_transform( Xf ) )
+        resdf = pd.DataFrame( Uf )
+        resdf .index = index_labels
+        resdf .columns = ['UMAP.'+str(i) for i in range(umap_dimension)]
+    else :
+        print ( "FATAL : NO OTHER DR METHOD SPECIFIED/IMPLEMENTED" )
+        print ( "      : SET bDoUmap TO True IN create_mapping" )
+        exit(1)
     #
-    resdf = pd.DataFrame( np.array([ Xf.T[i] for i in range( len(Xf.T) ) if i<n_proj ]).T  ,
-                  index      = index_labels ,
-                  columns    = [ 'MFX.'+str(i) for i in range( len(Xf.T) ) if i<n_proj ] )
+    if not MF is None : # THIS IS NOT REALLY NEEDED
+        for i in range ( n_proj ) :
+            resdf.T.loc[ 'MFX.' + str(i) ] = Xf.T[i]
     #
-    for i in range(umap_dimension) :
-        resdf.loc[:,'UMAP.'+str(i)] = Uf.iloc[:,i].values
     if not clabels_o is None :
         resdf.loc[:,'cids.max']  = clabels_o
     if not clabels_n is None :
@@ -68,8 +75,8 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
         bVerbose:bool = False , bExtreme:bool = True , force_n_clusters:int = None ,
         n_components:int = None , bDoUmap:bool = True ,
         distance_type:str = 'correlation,spearman,absolute' ,
-        umap_dimension:int = 2 , umap_n_neighbors:int = 20 , umap_local_connectivity:float = 20. ,
-        umap_seed:int = 42 , hierarchy_cmd:str = 'max' , divergence = lambda r : np.exp(r) ,
+        umap_dimension:int = 2 , umap_n_neighbors:int = 20 , umap_local_connectivity:float = 2. ,
+        umap_seed:int = 42 , hierarchy_cmd:str = 'max' , divergence = lambda r : np.exp(r*2) ,
         add_labels:list[str] = None , sample_label:str = None , alignment_label:str = None ,
 	n_projections:int = 2 , directory:str = None ) -> tuple[pd.DataFrame] :
     #
@@ -83,13 +90,13 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
     #
     adf = adf.iloc[ np.inf != np.abs( 1.0/np.std(adf.values,1) ) ,
                     np.inf != np.abs( 1.0/np.std(adf.values,0) ) ].copy()
-    jdf = jdf .loc[ :,adf.columns.values.tolist() ].copy()
+    jdf = jdf.loc [ :,adf.columns.values.tolist() ].copy()
     #
     n_neighbors		= umap_n_neighbors
     local_connectivity	= umap_local_connectivity
     transform_seed	= umap_seed
     cmd			= hierarchy_cmd
-    bRemoveCurse_	= True
+    bRemoveCurse_	= True # False
     nRound_		= None
     #
     from impetuous.special import zvals
@@ -115,7 +122,7 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
                      bDoUmap     = bDoUmap     , umap_dimension = umap_dimension,
                      n_neighbors = n_neighbors , local_connectivity = local_connectivity ,
                      transform_seed = transform_seed, n_proj = n_projections )
-
+    #
     if bVerbose :
         print ( 'STORING RESULTS > ', 'resdf_f.tsv , soldf_f.tsv , hierarch_f.tsv' )
         resdf_f .to_csv( header_str + 'resdf_f.tsv' , sep='\t' )
@@ -141,8 +148,11 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
     #
     pcas_df , pcaw_df = None , None
     if not jdf is None :
-        if not ( sample_label is None or alignment_label is None ) :
+        if not ( alignment_label is None ) :
             from impetuous.quantification import multivariate_aligned_pca
+            if sample_label is None :
+                jdf.loc['samplenames'] = jdf.columns.values
+                sample_label = 'samplenames'
             pcas_df , pcaw_df = multivariate_aligned_pca ( adf , jdf ,
                     sample_label = sample_label , align_to = alignment_label ,
                     n_components = n_components , add_labels = add_labels )
@@ -150,7 +160,6 @@ def full_mapping ( adf:pd.DataFrame , jdf:pd.DataFrame ,
                 print ( 'STORING RESULTS > ', 'pcas_df.tsv', 'pcaw_df.tsv' )
                 pcas_df .to_csv( header_str + 'pcas_df.tsv','\t' )
                 pcaw_df .to_csv( header_str + 'pcaw_df.tsv','\t' )
-
             resdf_f = pd.concat( [resdf_f.T, pcas_df.T] ).T
             resdf_s = pd.concat( [resdf_s.T, pcaw_df.T] ).T
     #
@@ -170,7 +179,7 @@ if __name__ == '__main__' :
     jdf = pd.read_csv('journal.tsv',sep='\t',index_col=0)
     jdf = jdf.loc[:,adf.columns.values]
     #
-    # adf=adf.iloc[:200,:]
+    #adf = adf.iloc[:200,:]
     alignment_label , sample_label = 'Disease' , 'sample'
     add_labels = ['Cell-line']
     #
@@ -182,7 +191,7 @@ if __name__ == '__main__' :
     bDoUmap            = True
     umap_dimension     = 2
     n_neighbors        = 20.
-    local_connectivity = 20.
+    local_connectivity = 2.
     transform_seed     = 42
     #
     print ( adf , jdf )
@@ -204,7 +213,7 @@ if __name__ == '__main__' :
         umap_seed = transform_seed	,
 	hierarchy_cmd = cmd		,
         add_labels = add_labels		,
-	alignment_label = ''		,
-	sample_label = ''		)
+	alignment_label = alignment_label,
+	sample_label = None	)
 
 
