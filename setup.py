c1aaf092ea8dbac56d7cdefb8fd8f6303cba8a13
Richard Tjörnhammar <98884156+rictjo@users.noreply.github.com>
2023-01-04 11:17:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name         = "biocarta",
-    version      = "0.2.4",
+    version      = "0.2.5",
     author       = "Richard Tjörnhammar",
     author_email = "richard.tjornhammar@gmail.com",
     description  = "",
