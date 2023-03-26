73db08a81a000e2a77b6cd03f5b1202befeafd6b
Richard Tjörnhammar <98884156+rictjo@users.noreply.github.com>
2023-03-26 10:30:07 +0200

    biocarta becomes biocartograph

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name         = "biocarta",
+    name         = "biocartograph",
     version      = "0.2.27",
     author       = "Richard Tjörnhammar",
     author_email = "richard.tjornhammar@gmail.com",
-    description  = "",
+    description  = "Package was renamed from Biocarta v0.2.27 to Biocartograph because of an unintentional name clash",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/rictjo/biocarta",
     packages = setuptools.find_packages('src'),
-    package_dir = {'biocarta':'src/biocarta','quantification':'src/quantification','special':'src/special','enrichment':'src/enrichment'},
+    package_dir = {'biocartograph':'src/biocartograph','quantification':'src/quantification','special':'src/special','enrichment':'src/enrichment'},
     classifiers = [
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
